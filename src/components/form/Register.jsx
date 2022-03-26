dec1944d0b8aa4935dc167dae674466ee3a9d2cb
SoSmoothy <58429050+sonicname@users.noreply.github.com>
2022-03-26 13:31:10 +0700

    update final

--- src/components/form/Register.jsx
+++ src/components/form/Register.jsx
@@ -6,100 +6,154 @@ import CheckBoxHook from "../checkbox/CheckBoxHook";
 import DropDownHook from "../dropdown/DropDownHook";
 
 import { dropdownData } from "../../data/dropdownData";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { schema } from "../../schema/validate";
 
 const Register = () => {
   const {
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid, isSubmitting },
     control,
     setValue,
-  } = useForm();
+    reset,
+    watch,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+    defaultValues: {
+      gender: "male",
+    },
+  });
 
-  const handleSubmitForm = (value) => {
-    console.log(value);
+  const onSubmitHandler = () => {
+    if (!isValid) return;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+        reset({
+          username: "",
+          email: "",
+          password: "",
+          gender: "male",
+          job: "",
+          term: false,
+        });
+      }, 5000);
+    });
   };
-
+  const watchGender = watch("gender");
   return (
     <form
-      onSubmit={handleSubmit(handleSubmitForm)}
-      className="max-w-[300px] mx-auto mt-10 my-10"
+      onSubmit={handleSubmit(onSubmitHandler)}
+      className="max-w-[300px] mx-auto my-10"
+      autoComplete="off"
     >
-      {/* Username */}
       <div className="flex flex-col gap-3 mb-5">
-        <label className="cursor-pointer" htmlFor="username">
+        <label htmlFor="username" className="cursor-pointer">
           Username
         </label>
         <InputHook
           name="username"
+          placeholder="Enter your username"
           id="username"
-          placeholder="Enter Your Username"
           control={control}
           type="text"
         />
-        <p className="text-red-500 text-sm">Please Enter Your Username</p>
+        {errors.username && (
+          <p className="text-sm text-red-500">{errors.username.message}</p>
+        )}
       </div>
-      {/* Email */}
       <div className="flex flex-col gap-3 mb-5">
-        <label className="cursor-pointer " htmlFor="username">
+        <label htmlFor="email" className="cursor-pointer">
           Email address
         </label>
         <InputHook
           name="email"
+          placeholder="Enter your email"
           id="email"
-          placeholder="Enter Your Email Address"
           control={control}
           type="email"
         />
+        {errors.email && (
+          <p className="text-sm text-red-500">{errors.email.message}</p>
+        )}
       </div>
-      {/* Password */}
       <div className="flex flex-col gap-3 mb-5">
-        <label className="cursor-pointer " htmlFor="username">
+        <label htmlFor="password" className="cursor-pointer">
           Password
         </label>
         <InputHook
           name="password"
+          placeholder="Enter your password"
           id="password"
-          placeholder="Enter Your Password"
           control={control}
           type="password"
         />
+        {errors.password && (
+          <p className="text-sm text-red-500">{errors.password.message}</p>
+        )}
       </div>
-      {/* GENDER */}
       <div className="flex flex-col gap-3 mb-5">
         <label className="cursor-pointer">Gender</label>
         <div className="flex items-center gap-5">
           <div className="flex items-center gap-x-3">
-            <RadioHook control={control} name="gender" value="male" />
+            <RadioHook
+              control={control}
+              name="gender"
+              value="male"
+              checked={watchGender === "male"}
+            />
             <span>Male</span>
           </div>
           <div className="flex items-center gap-x-3">
-            <RadioHook control={control} name="gender" value="female" />
+            <RadioHook
+              control={control}
+              name="gender"
+              value="female"
+              checked={watchGender === "female"}
+            />
             <span>Female</span>
           </div>
         </div>
+        {errors.gender && (
+          <p className="text-sm text-red-500">{errors.gender.message}</p>
+        )}
       </div>
-      {/* Job dropdown */}
       <div className="flex flex-col gap-3 mb-5">
-        <label className="cursor-pointer">Are you?</label>
+        <label className="cursor-pointer">Are you</label>
         <DropDownHook
           control={control}
           setValue={setValue}
           name="job"
           data={dropdownData}
-          dropdownLabel={"Select Your Job"}
+          dropdownLabel="Select your job"
         />
+        {errors.job && (
+          <p className="text-sm text-red-500">{errors.job.message}</p>
+        )}
       </div>
-      {/* Terms */}
-      <div>
+      <div className="flex flex-col gap-3">
         <CheckBoxHook
           control={control}
           text="I accept the terms and conditions"
-          name="terms"
+          name="term"
         />
+        {errors.term && (
+          <p className="text-sm text-red-500">{errors.term.message}</p>
+        )}
       </div>
-      {/* Submit */}
-      <button className="w-full p-5 bg-blue-500 text-white rounded-lg mt-5 font-semibold">
-        Submit
+      <button
+        className={`w-full p-5 mt-5 font-semibold text-white bg-blue-500 rounded-lg ${
+          isSubmitting ? "opacity-50" : ""
+        }`}
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? (
+          <div className="w-5 h-5 mx-auto border-2 border-t-2 border-white rounded-full border-t-transparent animate-spin" />
+        ) : (
+          "Submit"
+        )}
       </button>
     </form>
   );
---
+++ src/schema/validate.jsx
@@ -0,0 +1,31 @@
+import * as yup from "yup";
+
+export const schema = yup
+  .object({
+    username: yup.string().required("Please enter your username"),
+    email: yup
+      .string()
+      .email("Please enter valid email address")
+      .required("Please enter your email address"),
+    password: yup
+      .string()
+      .min(8, "Your password must be at least 8 characters or greater")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        {
+          message:
+            "Your password must have at least 1 uppercase, 1 lowercase, 1 special character",
+        }
+      )
+      .required("Please enter your password"),
+    gender: yup
+      .string()
+      .required("Please select your gender")
+      .oneOf(["male", "female"], "You can only select male or female"),
+    job: yup
+      .string()
+      .required("Please select your job")
+      .oneOf(["teacher", "developer", "doctor", "constructor"]),
+    term: yup.boolean().required("Please accept the terms and conditions"),
+  })
+  .required();
